189be8efb4e8143b10868e4516d0fa01a1c84ac0
Andrew KIl <kil.andrew@gmail.com>
2019-08-19 18:12:13 -0400

    add alert when deal is clicked with $0 bet, remove deal and bet when deal is clicked, rename variables

--- game.js
+++ game.js
@@ -22,11 +22,10 @@ document.getElementById("start-button").addEventListener("click", function() {
 
   game.startGame();
 
-  const betHundredButton = document.createElement("button");
-  const betHundredButtonText = document.createTextNode("$100");
-  betHundredButton.setAttribute("id", "bet-hundred-button");
-  betHundredButton.appendChild(betHundredButtonText);
-  rootDiv.appendChild(betHundredButton);
+  const playerBank = document.createElement("div");
+  const playerBankText = document.createTextNode(`bank: $${game.player.bank}`);
+  playerBank.appendChild(playerBankText);
+  rootDiv.appendChild(playerBank);
 
   const betAmount = document.createElement("div");
   const betAmountText = document.createTextNode("bet: $0");
@@ -34,10 +33,11 @@ document.getElementById("start-button").addEventListener("click", function() {
   betAmount.appendChild(betAmountText);
   rootDiv.appendChild(betAmount);
 
-  const playerBank = document.createElement("div");
-  const playerBankText = document.createTextNode(`bank: $${game.player.bank}`);
-  playerBank.appendChild(playerBankText);
-  rootDiv.appendChild(playerBank);
+  const betHundredButton = document.createElement("button");
+  const betHundredButtonText = document.createTextNode("$100");
+  betHundredButton.setAttribute("id", "bet-hundred-button");
+  betHundredButton.appendChild(betHundredButtonText);
+  rootDiv.appendChild(betHundredButton);
 
   let totalBet = 0;
   document
@@ -50,89 +50,98 @@ document.getElementById("start-button").addEventListener("click", function() {
       playerBankText.nodeValue = `bank: $${game.player.bank}`;
     });
 
-  const playButton = document.createElement("button");
-  const playButtonText = document.createTextNode("play");
-  playButton.setAttribute("id", "play-button");
-  playButton.appendChild(playButtonText);
-  rootDiv.appendChild(playButton);
+  const dealButton = document.createElement("button");
+  const dealButtonText = document.createTextNode("deal");
+  dealButton.setAttribute("id", "deal-button");
+  dealButton.appendChild(dealButtonText);
+  rootDiv.appendChild(dealButton);
 
-  document.getElementById("play-button").addEventListener("click", function() {
-    game.playGame();
+  document.getElementById("deal-button").addEventListener("click", function() {
+    if (game.player.betAmount === 0) {
+      window.alert("please place a bet");
+    } else {
+      rootDiv.removeChild(document.getElementById("deal-button"));
+      rootDiv.removeChild(document.getElementById("bet-hundred-button"));
 
-    const dealerCardsDiv = document.createElement("div");
-    const playerCardsDiv = document.createElement("div");
+      game.playGame();
 
-    const dealerScoreDiv = document.createElement("div");
-    const playerScoreDiv = document.createElement("div");
+      const dealerCardsDiv = document.createElement("div");
+      const playerCardsDiv = document.createElement("div");
 
-    const dealerCardsText = document.createTextNode("dealer cards: ");
-    const playerCardsText = document.createTextNode("player cards: ");
+      const dealerScoreDiv = document.createElement("div");
+      const playerScoreDiv = document.createElement("div");
 
-    const dealerScoreText = document.createTextNode("dealer score: ");
-    const playerScoreText = document.createTextNode("player score: ");
+      const dealerCardsText = document.createTextNode("dealer cards: ");
+      const playerCardsText = document.createTextNode("player cards: ");
 
-    const dealerCardsNode = document.createTextNode(
-      game.dealer.hand.map(card => {
-        return `${card.value} of ${card.suit}`;
-      })
-    );
-    const playerCardsNode = document.createTextNode(
-      game.player.hand.map(card => {
-        return `${card.value} of ${card.suit}`;
-      })
-    );
+      const dealerScoreText = document.createTextNode("dealer score: ");
+      const playerScoreText = document.createTextNode("player score: ");
 
-    const dealerScoreNode = document.createTextNode(
-      game.dealer.calculateScore()
-    );
-    let playerScoreNode = document.createTextNode(game.player.calculateScore());
-
-    dealerCardsDiv.appendChild(dealerCardsText);
-    dealerCardsDiv.appendChild(dealerCardsNode);
-    dealerScoreDiv.appendChild(dealerScoreText);
-    dealerScoreDiv.appendChild(dealerScoreNode);
-    dealerDiv.appendChild(dealerCardsDiv);
-    dealerDiv.appendChild(dealerScoreDiv);
-    rootDiv.appendChild(dealerDiv);
-
-    playerCardsDiv.appendChild(playerCardsText);
-    playerCardsDiv.appendChild(playerCardsNode);
-    playerScoreDiv.appendChild(playerScoreText);
-    playerScoreDiv.appendChild(playerScoreNode);
-    playerDiv.appendChild(playerCardsDiv);
-    playerDiv.appendChild(playerScoreDiv);
-    rootDiv.appendChild(playerDiv);
-
-    const standButton = document.createElement("button");
-    const standButtonText = document.createTextNode("stand");
-    standButton.setAttribute("id", "stand-button");
-    standButton.appendChild(standButtonText);
-    rootDiv.appendChild(standButton);
-
-    document
-      .getElementById("stand-button")
-      .addEventListener("click", function() {
-        game.player.stand();
-      });
-
-    const hitButton = document.createElement("button");
-    const hitButtonText = document.createTextNode("hit");
-    hitButton.setAttribute("id", "hit-button");
-    hitButton.appendChild(hitButtonText);
-    rootDiv.appendChild(hitButton);
-
-    document.getElementById("hit-button").addEventListener("click", function() {
-      game.player.hit();
-
-      const lastCard = game.player.hand[game.player.hand.length - 1];
-      const playerNewCardNode = document.createTextNode(
-        `,${lastCard.value} of ${lastCard.suit}`
+      const dealerCardsNode = document.createTextNode(
+        game.dealer.hand.map(card => {
+          return `${card.value} of ${card.suit}`;
+        })
       );
-      playerCardsDiv.appendChild(playerNewCardNode);
+      const playerCardsNode = document.createTextNode(
+        game.player.hand.map(card => {
+          return `${card.value} of ${card.suit}`;
+        })
+      );
 
-      playerScoreNode.nodeValue = `${game.player.calculateScore()}`;
-    });
+      const dealerScoreNode = document.createTextNode(
+        game.dealer.calculateScore()
+      );
+      let playerScoreNode = document.createTextNode(
+        game.player.calculateScore()
+      );
+
+      dealerCardsDiv.appendChild(dealerCardsText);
+      dealerCardsDiv.appendChild(dealerCardsNode);
+      dealerScoreDiv.appendChild(dealerScoreText);
+      dealerScoreDiv.appendChild(dealerScoreNode);
+      dealerDiv.appendChild(dealerCardsDiv);
+      dealerDiv.appendChild(dealerScoreDiv);
+      rootDiv.appendChild(dealerDiv);
+
+      playerCardsDiv.appendChild(playerCardsText);
+      playerCardsDiv.appendChild(playerCardsNode);
+      playerScoreDiv.appendChild(playerScoreText);
+      playerScoreDiv.appendChild(playerScoreNode);
+      playerDiv.appendChild(playerCardsDiv);
+      playerDiv.appendChild(playerScoreDiv);
+      rootDiv.appendChild(playerDiv);
+
+      const standButton = document.createElement("button");
+      const standButtonText = document.createTextNode("stand");
+      standButton.setAttribute("id", "stand-button");
+      standButton.appendChild(standButtonText);
+      rootDiv.appendChild(standButton);
+
+      document
+        .getElementById("stand-button")
+        .addEventListener("click", function() {
+          game.player.stand();
+        });
+
+      const hitButton = document.createElement("button");
+      const hitButtonText = document.createTextNode("hit");
+      hitButton.setAttribute("id", "hit-button");
+      hitButton.appendChild(hitButtonText);
+      rootDiv.appendChild(hitButton);
+
+      document
+        .getElementById("hit-button")
+        .addEventListener("click", function() {
+          game.player.hit();
+
+          const lastCard = game.player.hand[game.player.hand.length - 1];
+          const playerNewCardNode = document.createTextNode(
+            `,${lastCard.value} of ${lastCard.suit}`
+          );
+          playerCardsDiv.appendChild(playerNewCardNode);
+
+          playerScoreNode.nodeValue = `${game.player.calculateScore()}`;
+        });
+    }
   });
-
-  // game.bet();
 });
