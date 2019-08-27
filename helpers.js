98993d1a94af8b5499856dc537f8dcec155bb712
Andrew KIl <kil.andrew@gmail.com>
2019-08-27 15:03:05 -0400

    restructure code: html instead of js dom manipulation

--- helpers.js
+++ helpers.js
@@ -1,65 +1,3 @@
-const buttonMaker = (label, id = label) => {
-  const button = document.createElement("button");
-  button.innerHTML = label;
-  button.setAttribute("id", `${id}-button`);
-  return button;
-};
-
-export const createStartButton = rootElement => {
-  const startButton = buttonMaker("start");
-  rootElement.appendChild(startButton);
-};
-
-export const createDealButton = rootElement => {
-  const dealButton = buttonMaker("deal");
-  rootElement.appendChild(dealButton);
-};
-
-export const createStandButton = rootElement => {
-  const standButton = buttonMaker("stand");
-  rootElement.appendChild(standButton);
-};
-
-export const createBetOneButton = rootElement => {
-  const betHundredButton = buttonMaker("$1", "bet-one");
-  rootElement.appendChild(betHundredButton);
-};
-
-export const createBetFiveButton = rootElement => {
-  const betFiveButton = buttonMaker("$5", "bet-five");
-  rootElement.appendChild(betFiveButton);
-};
-
-export const createBetTwentyFiveButton = rootElement => {
-  const betTwentyFiveButton = buttonMaker("$25", "bet-twenty-five");
-  rootElement.appendChild(betTwentyFiveButton);
-};
-
-export const createBetHundredButton = rootElement => {
-  const betHundredButton = buttonMaker("$100", "bet-hundred");
-  rootElement.appendChild(betHundredButton);
-};
-
-export const createBetFiveHundredButton = rootElement => {
-  const betFiveHundredButton = buttonMaker("$500", "bet-five-hundred");
-  rootElement.appendChild(betFiveHundredButton);
-};
-
-export const createBetThousandButton = rootElement => {
-  const betThousandButton = buttonMaker("$1000", "bet-thousand");
-  rootElement.appendChild(betThousandButton);
-};
-
-export const createHitButton = rootElement => {
-  const hitButton = buttonMaker("hit");
-  rootElement.appendChild(hitButton);
-};
-
-export const createNewGameButton = rootElement => {
-  const newGameButton = buttonMaker("new-game");
-  rootElement.appendChild(newGameButton);
-};
-
 export const disable = button => {
   document.getElementById(button).disabled = true;
 };
@@ -75,6 +13,13 @@ export const disableAllBets = () => {
   document.getElementById("bet-hundred-button").disabled = true;
   document.getElementById("bet-five-hundred-button").disabled = true;
   document.getElementById("bet-thousand-button").disabled = true;
+};
+
+export const disableAllActions = () => {
+  document.getElementById("deal-button").disabled = true;
+  document.getElementById("stand-button").disabled = true;
+  document.getElementById("hit-button").disabled = true;
+  document.getElementById("new-game-button").disabled = true;
 };
 
 export const enableAllBets = () => {
