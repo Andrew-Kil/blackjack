a36fa5650f3d5bc9f74aa5888020b32cd7ae5337
Andrew KIl <kil.andrew@gmail.com>
2019-09-08 10:35:52 -0400

    fix hit logic

--- index.js
+++ index.js
@@ -171,13 +171,13 @@ const addClickToHitButton = () =>
     updatePlayerCardsAndScore();
     game.player.calculateScore();
 
-    if (game.player.score >= 21) {
+    if (game.player.score > 21) {
       game.processBets();
       updateBetAndBank(game.player.bank, game.player.betAmount);
 
-      disable("stand-button");
+      // disable("stand-button");
       disable("hit-button");
-      enable("new-game-button");
+      // enable("new-game-button");
     } else {
       enable("stand-button");
       enable("hit-button");
