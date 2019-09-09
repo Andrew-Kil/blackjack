cc143757f3c94db71d0ef8bbe0540dcd421cdb8e
Andrew KIl <kil.andrew@gmail.com>
2019-09-09 18:20:20 -0400

    update styling

--- helpers.js
+++ helpers.js
@@ -1,12 +1,12 @@
-export const disable = button => {
+export const hide = button => {
   document.getElementById(button).style.visibility = "hidden";
 };
 
-export const enable = button => {
+export const show = button => {
   document.getElementById(button).style.visibility = "visible";
 };
 
-export const disableAllBets = () => {
+export const hideAllBets = () => {
   document.getElementById("bet-one-button").style.visibility = "hidden";
   document.getElementById("bet-five-button").style.visibility = "hidden";
   document.getElementById("bet-twenty-five-button").style.visibility = "hidden";
@@ -16,14 +16,14 @@ export const disableAllBets = () => {
   document.getElementById("bet-thousand-button").style.visibility = "hidden";
 };
 
-export const disableAllActions = () => {
+export const hideAllActions = () => {
   document.getElementById("deal-button").style.visibility = "hidden";
   document.getElementById("stand-button").style.visibility = "hidden";
   document.getElementById("hit-button").style.visibility = "hidden";
   document.getElementById("new-game-button").style.visibility = "hidden";
 };
 
-export const enableAllBets = () => {
+export const showAllBets = () => {
   document.getElementById("bet-one-button").style.visibility = "visible";
   document.getElementById("bet-five-button").style.visibility = "visible";
   document.getElementById("bet-twenty-five-button").style.visibility =
