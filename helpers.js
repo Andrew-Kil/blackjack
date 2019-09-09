17f6a7cdb42575cf44c0cf903fa0e20b97c44d92
Andrew KIl <kil.andrew@gmail.com>
2019-09-09 11:00:46 -0400

    change buttons from disable/enable to visible/hidden

--- helpers.js
+++ helpers.js
@@ -1,32 +1,35 @@
 export const disable = button => {
-  document.getElementById(button).disabled = true;
+  document.getElementById(button).style.visibility = "hidden";
 };
 
 export const enable = button => {
-  document.getElementById(button).disabled = false;
+  document.getElementById(button).style.visibility = "visible";
 };
 
 export const disableAllBets = () => {
-  document.getElementById("bet-one-button").disabled = true;
-  document.getElementById("bet-five-button").disabled = true;
-  document.getElementById("bet-twenty-five-button").disabled = true;
-  document.getElementById("bet-hundred-button").disabled = true;
-  document.getElementById("bet-five-hundred-button").disabled = true;
-  document.getElementById("bet-thousand-button").disabled = true;
+  document.getElementById("bet-one-button").style.visibility = "hidden";
+  document.getElementById("bet-five-button").style.visibility = "hidden";
+  document.getElementById("bet-twenty-five-button").style.visibility = "hidden";
+  document.getElementById("bet-hundred-button").style.visibility = "hidden";
+  document.getElementById("bet-five-hundred-button").style.visibility =
+    "hidden";
+  document.getElementById("bet-thousand-button").style.visibility = "hidden";
 };
 
 export const disableAllActions = () => {
-  document.getElementById("deal-button").disabled = true;
-  document.getElementById("stand-button").disabled = true;
-  document.getElementById("hit-button").disabled = true;
-  document.getElementById("new-game-button").disabled = true;
+  document.getElementById("deal-button").style.visibility = "hidden";
+  document.getElementById("stand-button").style.visibility = "hidden";
+  document.getElementById("hit-button").style.visibility = "hidden";
+  document.getElementById("new-game-button").style.visibility = "hidden";
 };
 
 export const enableAllBets = () => {
-  document.getElementById("bet-one-button").disabled = false;
-  document.getElementById("bet-five-button").disabled = false;
-  document.getElementById("bet-twenty-five-button").disabled = false;
-  document.getElementById("bet-hundred-button").disabled = false;
-  document.getElementById("bet-five-hundred-button").disabled = false;
-  document.getElementById("bet-thousand-button").disabled = false;
+  document.getElementById("bet-one-button").style.visibility = "visible";
+  document.getElementById("bet-five-button").style.visibility = "visible";
+  document.getElementById("bet-twenty-five-button").style.visibility =
+    "visible";
+  document.getElementById("bet-hundred-button").style.visibility = "visible";
+  document.getElementById("bet-five-hundred-button").style.visibility =
+    "visible";
+  document.getElementById("bet-thousand-button").style.visibility = "visible";
 };
